ad849af0a7c9e171ab162360f56f99cd4148a346
hainh1712 <nguyenhoanghai20021712@gmail.com>
2024-03-15 10:52:46 +0700

    lcp with next14

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["cdn.tgdd.vn", "danviet.mediacdn.vn", "image.plo.vn", "itss-hedsocial.s3.amazonaws.com"], 
+  },
 };
 
 export default nextConfig;
